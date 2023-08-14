bc6f4aea40ec37a51aa7fac29832959f70637cc0
smiling <smiling@smilingdeMacBook-Air.local>
2023-08-14 19:07:38 +0800

    update

---
+++ examples/llama2/README.md
@@ -0,0 +1,20 @@
+# Pipeline for the creation of llama2.
+There were three main steps to the training process:
+1. Pretrain the base model to enhance chinese:
+    - `python train_tokenizer.py --model_prefiex --input_file --output_sp_dir --output_hf_dir --llama_tokenizer_dir`
+
+2. Supervised fine-tuning of the base model to create dialogue_sft:
+    - `torchrun --nnodes 1  --nproc_per_node 1 supervised_finetuning.py --model_path /root/autodl-tmp/chatglm2-6b --dataset_name /root/autodl-tmp/dialoguesum/test.csv --learning_rate 1e-5 --max_steps 5000 --output_dir /root/autodl-tmp/tuning/dialogue_sft`
+
+3. RL fine-tuning of llama-7b-se with the llama-7b-se-rm reward model:
+    - `accelerate launch --multi_gpu --num_machines 1  --num_processes 8 examples/stack_llama/scripts/rl_training.py --log_with=wandb --model_name=<LLAMA_SE_MODEL> --reward_model_name=<LLAMA_SE_RM_MODEL> --adafactor=False --tokenizer_name=<LLAMA_TOKENIZER> --save_freq=100 --output_max_length=128 --batch_size=8 --gradient_accumulation_steps=8 --batched_gen=True --ppo_epochs=4 --seed=0 --learning_rate=1.4e-5 --early_stopping=True --output_dir=llama-se-rl-finetune-128-8-8-1.4e-5_adam`
+
+
+LoRA layers were using at all stages to reduce memory requirements. 
+At each stage the peft adapter layers were merged with the base model, using: 
+```shell
+python examples/stack_llama/scripts/merge_peft_adapter.py --adapter_model_name=XXX --base_model_name=YYY --output_name=ZZZ
+```
+Note that this script requires `peft>=0.3.0`.
+
+For access to the base llama-7b model, please see Meta's [release](https://ai.facebook.com/blog/large-language-model-llama-meta-ai/) and [request form](https://docs.google.com/forms/d/e/1FAIpQLSfqNECQnMkycAp2jP4Z9TFX0cGR4uf7b_fBxjY_OjhJILlKGA/viewform).
---
+++ examples/llama2/train_tokenizer.py
@@ -0,0 +1,89 @@
+import sentencepiece as spm
+from transformers import LlamaTokenizer
+from sentencepiece import sentencepiece_model_pb2 as sp_pb2_model
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_prefix", type=str, default="chinese_sp")
+    parser.add_argument("--input_file", type=str, default="")
+
+    parser.add_argument('--llama_tokenizer_dir', default=None, type=str, required=True)
+    parser.add_argument('--output_sp_dir', default='', type=str)
+    parser.add_argument('--output_hf_dir', default='', type=str)
+    return parser.parse_args()
+
+def train(input_file, model_prefix):
+    # Define the vocab size and special tokens
+    vocab_size = 200000
+    special_tokens = ['<PAD>', '<UNK>', '<s>', '</s>']
+
+    # Train the SentencePiece model
+    spm.SentencePieceTrainer.train(
+        input=input_file,
+        model_prefix=model_prefix,
+        vocab_size=vocab_size,
+        user_defined_symbols=special_tokens
+    )
+
+    print(f"Tokenizer trained and saved to {model_prefix}.model")
+    
+def merge(llama_tokenizer_dir, model_prefix, output_sp_dir, output_hf_dir):
+    # load
+    chinese_sp_model_file = model_prefix + '.model'
+    llama_tokenizer = LlamaTokenizer.from_pretrained(llama_tokenizer_dir)
+    chinese_sp_model = spm.SentencePieceProcessor()
+    chinese_sp_model.Load(chinese_sp_model_file)
+
+    llama_spm = sp_pb2_model.ModelProto()
+    llama_spm.ParseFromString(llama_tokenizer.sp_model.serialized_model_proto())
+    chinese_spm = sp_pb2_model.ModelProto()
+    chinese_spm.ParseFromString(chinese_sp_model.serialized_model_proto())
+
+    # print number of tokens
+    print(len(llama_tokenizer),len(chinese_sp_model))
+    print(llama_tokenizer.all_special_tokens)
+    print(llama_tokenizer.all_special_ids)
+    print(llama_tokenizer.special_tokens_map)
+
+    ## Add Chinese tokens to LLaMA tokenizer
+    llama_spm_tokens_set=set(p.piece for p in llama_spm.pieces)
+    print(len(llama_spm_tokens_set))
+    print(f"Before:{len(llama_spm_tokens_set)}")
+    for p in chinese_spm.pieces:
+        piece = p.piece
+        if piece not in llama_spm_tokens_set:
+            new_p = sp_pb2_model.ModelProto().SentencePiece()
+            new_p.piece = piece
+            new_p.score = 0
+            llama_spm.pieces.append(new_p)
+    print(f"New model pieces: {len(llama_spm.pieces)}")
+
+    ## Save
+    os.makedirs(output_sp_dir,exist_ok=True)
+    with open(output_sp_dir+'/chinese_llama.model', 'wb') as f:
+        f.write(llama_spm.SerializeToString())
+    tokenizer = LlamaTokenizer(vocab_file=output_sp_dir+'/chinese_llama.model')
+
+    tokenizer.save_pretrained(output_hf_dir)
+    print(f"Chinese-LLaMA tokenizer has been saved to {output_hf_dir}")
+    
+def test(llama_tokenizer_dir, output_hf_dir):
+    # Test
+    llama_tokenizer = LlamaTokenizer.from_pretrained(llama_tokenizer_dir)
+    chinese_llama_tokenizer = LlamaTokenizer.from_pretrained(output_hf_dir)
+    print(tokenizer.all_special_tokens)
+    print(tokenizer.all_special_ids)
+    print(tokenizer.special_tokens_map)
+    text='''白日依山尽，黄河入海流。欲穷千里目，更上一层楼。
+    The primary use of LLaMA is research on large language models, including'''
+    print("Test text:\n",text)
+    print(f"Tokenized by LLaMA tokenizer:{llama_tokenizer.tokenize(text)}")
+    print(f"Tokenized by Chinese-LLaMA tokenizer:{chinese_llama_tokenizer.tokenize(text)}")
+    
+def __init__ == "__main__":
+    args = get_args()
+    train(args.input_file, args.model_prefix)
+    merge(args.llama_tokenizer_dir, args.model_prefix, args.output_sp_dir, args.output_hf_dir)
+    test(args.llama_tokenizer_dir, args.output_hf_dir)
+    
